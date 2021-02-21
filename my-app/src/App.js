714ca1a98004e4e491b9556b4b8633892f1fe759
ari sori <adsoriano@scu.edu>
2021-02-20 17:29:40 -0800

    react app

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import React, {Component} from 'react';
+import './App.css';
+import Avatar from 'avataaars'
+import customAva from './components/customAva.js'
+import {Piece} from 'avataaars';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hey Boi
+      </header>
+
+
+      <customAva/>
+
+
+    </div>
+  );
+}
+
+export default App;
